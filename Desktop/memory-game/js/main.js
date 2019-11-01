368609a0a2a311bf76d63618e0cc3413b1ddbb52
Chunrong <56704548+wangchunrong1983@users.noreply.github.com>
2019-10-31 22:43:33 -0700

    JSON

--- Desktop/memory-game/js/main.js
+++ Desktop/memory-game/js/main.js
@@ -1,5 +1,22 @@
 
-let cards =["queen","queen","king","king"];
+let cards =[
+{rank : "queen",
+ suit : "hearts",
+ cardImage: "images/queen-of-hearts.png"
+},
+{rank : "queen",
+ suit : "diamonds",
+ cardImage: "images/queen-of-diamonds.png"
+},
+{rank: "king",
+suit: "hearts",
+cardImage : "images/king-of-hearts.png"
+},
+{rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}
+];
 
 let cardsInPlay=[];
 
@@ -7,17 +24,20 @@ let cardsInPlay=[];
 function checkForMatch(){
 
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	  console.log("You found a match!");
+	  alert("You found a match!");
 	} 
 	else {
-	  console.log("Sorry, try again.");
+	  alert("Sorry, try again.");
 	}
 };
 
 function flipCard(cardId){
 
-	console.log("User flipped" + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped" + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
 	checkForMatch();
 
@@ -25,6 +45,8 @@ function flipCard(cardId){
 
 flipCard(0);
 flipCard(2);
+
+
 
 
 
